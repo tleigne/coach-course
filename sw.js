84c720381ef09b46b58f33ef436aa96c371491e1
Thibault <t.leigne@gmail.com>
2026-07-20 23:27:28 +0200

    Rend l'appli installable : manifest PWA, service worker hors-ligne, icones

---
+++ sw.js
@@ -0,0 +1,52 @@
+// Service worker : mise en cache des fichiers de l'appli pour un fonctionnement hors-ligne.
+// Change ce numéro de version à chaque mise à jour du code pour forcer le rafraîchissement du cache.
+const CACHE_VERSION = 'coach-course-v1';
+
+const FICHIERS_A_METTRE_EN_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './js/app.js',
+  './js/gpx.js',
+  './js/geo.js',
+  './js/coach.js',
+  './js/utils.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(FICHIERS_A_METTRE_EN_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((noms) =>
+      Promise.all(noms.filter((nom) => nom !== CACHE_VERSION).map((nom) => caches.delete(nom)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((reponseEnCache) => {
+      if (reponseEnCache) return reponseEnCache;
+
+      return fetch(event.request)
+        .then((reponseReseau) => {
+          const copie = reponseReseau.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copie));
+          return reponseReseau;
+        })
+        .catch(() => reponseEnCache);
+    })
+  );
+});
